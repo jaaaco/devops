0e0dff06eebc5c4c6a21e2f6fc8bffac9a7e92f6
Jakub Wietrzyk <jakub@wietrzyk.com>
2016-01-26 10:25:26 +0000

    nginx proxy

--- nginx/init.sh
+++ nginx/init.sh
@@ -1,3 +1,12 @@
 apt-get update
-apt-get upgrade -i
+apt-get upgrade -y
 apt-get install nginx
+
+cp nginx.conf /etc/nginx.conf
+cp apache-forward /etc/nginx/sites-enabled/
+cp faillover /etc/nginx/sites-enabled/
+cp www/* /var/www/
+rm /etc/nginx/sites-enabled/default
+service nginx restart
+chown www-data /var/www/*
+chmod 0600 /var/www/*
